1399b3ad9ba6a5301b5d62f152c0c7d214299dec
Jeff St. Jean <jeffrey@stjean.ca>
2024-01-23 13:49:22 -0500

    Refactor Video to block when fetching next frame

--- vision_system/Video.py
+++ vision_system/Video.py
@@ -1,29 +1,36 @@
 import queue
-from threading import Thread
+from threading import Thread, Condition
 
 import cv2
 
 class Video:
-    conn = None
+    conn: str = None
     thread = None
     cap = None
-    frames = queue.Queue() # TODO: make this a circular buffer
-    width = None
-    height = None
+    width: int = None
+    height: int = None
+    frame = None
+    debug_print: bool = False
+    cv = Condition()
+    new_frame_available = False
 
-    def __init__(self, conn):
+    def __init__(self, conn: str, debug_print: bool = False, save_file: str = None):
         self.conn = conn
+        self.debug_print = debug_print
 
-        print(f"Connecting to {self.conn}")
+        if debug_print:
+            print(f"Connecting to {self.conn}")
         self.cap = cv2.VideoCapture(self.conn)
         if (self.cap.isOpened() == False):
             raise Exception(f"Could not connect to stream {self.conn}")
         
         self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        print(f"Connected to {self.conn} (Frame size: {self.width}x{self.height})")
 
-        self.thread = Thread(target=self.capture_video)
+        if debug_print:
+            print(f"Connected to {self.conn} (Frame size: {self.width}x{self.height})")
+
+        self.thread = Thread(target=self.capture_video, args=(self.cv, save_file))
         self.thread.daemon = True; # makes sure it cleans up on ctrl+c
         self.thread.start()
 
@@ -32,14 +39,16 @@ class Video:
             ret, frame = self.cap.read()
             if(ret == False):
                 print(f"Error receiving frame from stream {self.conn}")
-            else:
-                while True:
-                    try:
-                        self.frames.get(block=False)
-                    except queue.Empty:
-                        break
-                    pass
-                self.frames.put(frame)
+                continue
+
+            self.frame = frame
+
+            # notify that a new frame is available
+            self.new_frame_available = True
+            self.condition.notify()
+
+            # wait until frame has been retreived by user
+            self.condition.wait()
 
     def get_resolution(self):
         return (self.width, self.height)
@@ -50,3 +59,14 @@ class Video:
     
     def cleanup(self):
         self.cap.release()
+
+    def get_frame(self):
+        with self.condition:
+            if not self.new_frame_available:
+                self.condition.wait()  # Wait until a new frame is available
+
+            self.new_frame_available = False
+
+            # Notify the capture thread that it can capture the next frame
+            self.condition.notify()
+            return self.frame
